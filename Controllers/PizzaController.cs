ab851ceed8e37c17fd53547d075019c47827e381
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-07-14 19:41:01 +0200

    milestone 2 added

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace LaMiaPizzeria.Controllers
 {
     public class PizzaController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IngredientiPizza()
         {
             return View();
         }
