aee0da0b8e0551fe96040348201539fb868cebdd
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-07-14 14:03:30 +0200

    Aggiungere i file di progetto.

---
+++ Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace LaMiaPizzeria
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public double Price { get; set; }
+
+        public Pizza(string name, string description, string photoUrl, double price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.PhotoUrl = photoUrl;
+            this.Price = price;
+        }
+
+    }
+}
